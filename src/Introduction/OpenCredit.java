4b45e40d7fbf5b6fc1430f53fea25e97c9e9314f
Md.Moshiur Rahman <35825505+mhridoy@users.noreply.github.com>
2022-10-12 01:01:35 +0600

    Update OpenCredit.java

--- src/Introduction/OpenCredit.java
+++ src/Introduction/OpenCredit.java
@@ -1,8 +1,30 @@
-public class OpenCredit{
-  
- public static void main(){
- 
- 
- 
- } 
+package test;
+
+import java.util.Scanner;
+
+public class OpenCreditSystem {
+    public static void main(String[] args) {
+        try(Scanner in = new Scanner (System.in)){
+           int  testCase = in.nextInt();
+
+           while(testCase--!=0){
+            int numOfStudents = in.nextInt();
+            int max = in.nextInt();
+
+            int ans = -Integer.MAX_VALUE;
+
+            for (int i = 1; i < numOfStudents; i++) {
+                
+                int x = in.nextInt();
+
+                if (max - x > ans)
+				ans = max - x;
+			if (x > max)
+				max = x ;
+            }
+            
+            System.out.println(ans);
+           }
+        }
+    }
 }
